78d7da60c97ae1104865fdd3aabdd70e6624be8a
Darshan.V <darshanv487@gmail.com>
2024-04-23 13:55:33 +0530

    leart about enums in typescript

---
+++ Tuples/myTuples.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const user:(string | number | boolean)[]=[23,"jsjkjs"]
+var user;
+user = ["hello", 23, false];
+var newUser = [1, "Darshan"];
+newUser[0] = 23;
+newUser[1] = "darshanv487@gmail.com";
---
+++ Tuples/myTuples.ts
@@ -0,0 +1,14 @@
+// const user:(string | number | boolean)[]=[23,"jsjkjs"]
+let user: [string, number, boolean];
+
+user = ["hello", 23, false];
+
+
+type User=[number,string]
+
+const newUser:User=[1,"Darshan"]
+
+newUser[0]=23
+newUser[1]="darshanv487@gmail.com"
+// newUser.push(true)
+export {};
